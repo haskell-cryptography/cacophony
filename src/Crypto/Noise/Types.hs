32870e4a4a093f1cb087fddbd89acf3c012b3ece
John Galt <centromere@users.noreply.github.com>
2015-11-08 18:55:44 -0500

    Added documentation in Crypto.Noise.Types

--- src/Crypto/Noise/Types.hs
+++ src/Crypto/Noise/Types.hs
@@ -24,20 +24,26 @@ import qualified Data.ByteString as BS (ByteString)
 import qualified Data.ByteString.Lazy as BL (ByteString, toStrict, fromStrict)
 import Prelude hiding (concat)
 
+-- | Concatenates a list of 'ScrubbedBytes'.
 concatSB :: [ScrubbedBytes] -> ScrubbedBytes
 concatSB = concat
 
+-- | Converts a lazy ByteString to ScrubbedBytes.
 bsToSB :: BL.ByteString -> ScrubbedBytes
 bsToSB = convert . BL.toStrict
 
+-- | Strict version of 'bsToSB'.
 bsToSB' :: BS.ByteString -> ScrubbedBytes
 bsToSB' = convert
 
+-- | Converts ScrubbedBytes to a lazy ByteString.
 sbToBS :: ScrubbedBytes -> BL.ByteString
 sbToBS = BL.fromStrict . convert
 
+-- | Strict version of 'sbToBS''.
 sbToBS' :: ScrubbedBytes -> BS.ByteString
 sbToBS' = convert
 
+-- | Equality operator for 'ScrubbedBytes'.
 sbEq :: ScrubbedBytes -> ScrubbedBytes -> Bool
 sbEq = eq

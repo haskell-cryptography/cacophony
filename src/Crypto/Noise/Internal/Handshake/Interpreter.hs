274e1b6794b8d3832ba3b26de2bba16987fa00b1
John Galt <centromere@users.noreply.github.com>
2017-05-18 03:26:06 -0400

    Add "e" token

--- src/Crypto/Noise/Internal/Handshake/Interpreter.hs
+++ src/Crypto/Noise/Internal/Handshake/Interpreter.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
 ----------------------------------------------------------------
 -- |
 -- Module      : Crypto.Noise.Internal.Handshake.Interpreter
@@ -7,31 +8,108 @@
 module Crypto.Noise.Internal.Handshake.Interpreter where
 
 import Control.Applicative.Free
+import Control.Exception.Safe
 import Control.Lens
+--import Control.Monad.Coroutine
+import Control.Monad.Coroutine.SuspensionFunctors
+import Data.ByteArray (splitAt)
+import Data.Monoid    ((<>))
+import Data.Proxy
+import Prelude hiding (splitAt)
 
-import Crypto.Noise.Internal.Handshake.Pattern
+import Crypto.Noise.Cipher
+import Crypto.Noise.DH
+import Crypto.Noise.Hash
+import Crypto.Noise.Internal.Handshake.Pattern hiding (e, s, ee, es, se, ss)
+import Crypto.Noise.Internal.Handshake.State
+import Crypto.Noise.Internal.SymmetricState
+import Crypto.Noise.Internal.Types
 
-interpretToken :: Token a -> IO a
-interpretToken (E  next) = return next
-interpretToken (S  next) = return next
-interpretToken (Ee next) = return next
-interpretToken (Es next) = return next
-interpretToken (Se next) = return next
-interpretToken (Ss next) = return next
+interpretToken :: forall c d h r. (Cipher c, DH d, Hash h)
+               => HandshakeRole
+               -> Token r
+               -> Handshake c d h r
+interpretToken opRole (E next) = do
+  myRole <- use $ hsOpts . hoRole
 
-runMessagePattern :: MessagePattern () -> IO ()
-runMessagePattern = runAp interpretToken
+  if opRole == myRole then do
+    (_, pk) <- getKeyPair hoLocalEphemeral "local ephemeral"
+    let pkBytes = dhPubToBytes pk
+    hsSymmetricState %= mixHash pkBytes
+    hsMsgBuffer      %= flip mappend pkBytes
 
-interpretMessage :: Message a -> IO a
+  else do
+    buf <- use hsMsgBuffer
+    let (pkBytes, remainingBytes) = splitAt (dhLength (Proxy :: Proxy d)) buf
+    theirKey <- maybe (throwM . HandshakeError $ "invalid remote ephemeral key")
+                      return
+                      (dhBytesToPub pkBytes)
+    hsOpts . hoRemoteEphemeral .= Just theirKey
+    hsSymmetricState           %= mixHash pkBytes
+    hsMsgBuffer                .= remainingBytes
+
+  return next
+interpretToken _ (S  next) = return next
+interpretToken _ (Ee next) = return next
+interpretToken _ (Es next) = return next
+interpretToken _ (Se next) = return next
+interpretToken _ (Ss next) = return next
+
+processMsgPattern :: (Cipher c, DH d, Hash h)
+                  => HandshakeRole
+                  -> MessagePattern r
+                  -> Handshake c d h r
+processMsgPattern opRole mp = do
+  myRole <- use $ hsOpts . hoRole
+  buf    <- use hsMsgBuffer
+  input  <- Handshake <$> request $ buf
+
+  if opRole == myRole then do
+    hsMsgBuffer .= mempty
+    r <- runAp (interpretToken opRole) mp
+    ss <- use hsSymmetricState
+    (encPayload, ss') <- encryptAndHash input ss
+    hsMsgBuffer      %= (flip mappend . cipherTextToBytes) encPayload
+    hsSymmetricState .= ss'
+    return r
+
+  else do
+    hsMsgBuffer .= input
+    r <- runAp (interpretToken opRole) mp
+    remaining <- use hsMsgBuffer
+    ss <- use hsSymmetricState
+    (decPayload, ss') <- decryptAndHash (cipherBytesToText remaining) ss
+    hsMsgBuffer .= decPayload
+    hsSymmetricState .= ss'
+    return r
+
+interpretMessage :: (Cipher c, DH d, Hash h)
+                 => Message r
+                 -> Handshake c d h r
 interpretMessage (PreInitiator _  next) = return next
 interpretMessage (PreResponder _  next) = return next
-interpretMessage (Initiator mp next) = do
-  runMessagePattern mp
-  return next
+interpretMessage (Initiator mp next) = processMsgPattern InitiatorRole mp >> return next
+interpretMessage (Responder mp next) = processMsgPattern ResponderRole mp >> return next
 
-interpretMessage (Responder mp next) = do
-  runMessagePattern mp
-  return next
-
-runHandshakePattern :: HandshakePattern -> IO ()
+runHandshakePattern :: (Cipher c, DH d, Hash h)
+                    => HandshakePattern
+                    -> Handshake c d h ()
 runHandshakePattern hp = runAp interpretMessage $ hp ^. hpMsgSeq
+
+getPublicKey :: Lens' (HandshakeOpts d) (Maybe (PublicKey d))
+             -> String
+             -> Handshake c d h (PublicKey d)
+getPublicKey k desc = do
+  r <- use $ hsOpts . k
+  maybe (throwM (InvalidHandshakeOptions $ "a " <> desc <> " key is required but has not been set"))
+        return
+        r
+
+getKeyPair :: Lens' (HandshakeOpts d) (Maybe (KeyPair d))
+           -> String
+           -> Handshake c d h (KeyPair d)
+getKeyPair k desc = do
+  r <- use $ hsOpts . k
+  maybe (throwM (InvalidHandshakeOptions $ "a " <> desc <> " key is required but has not been set"))
+        return
+        r
---
+++ src/Crypto/Noise/Internal/Handshake/State.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables, GeneralizedNewtypeDeriving,
+             FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+------------------------------------------------------
+-- |
+-- Module      : Crypto.Noise.Internal.Handshake.State
+-- Maintainer  : John Galt <jgalt@centromere.net>
+-- Stability   : experimental
+-- Portability : POSIX
+module Crypto.Noise.Internal.Handshake.State where
+
+import Control.Lens
+import Control.Monad.Coroutine
+import Control.Monad.Coroutine.SuspensionFunctors
+import Control.Monad.Catch.Pure
+import Control.Monad.State       (MonadState(..), StateT)
+import Control.Monad.Trans.Class (MonadTrans(lift))
+import Data.ByteArray            (ScrubbedBytes, convert)
+import Data.ByteString           (ByteString)
+import Data.Monoid               ((<>))
+import Data.Proxy
+
+import Crypto.Noise.Cipher
+import Crypto.Noise.DH
+import Crypto.Noise.Hash
+import Crypto.Noise.Internal.SymmetricState
+
+-- | Represents the side of the conversation upon which a party resides.
+data HandshakeRole = InitiatorRole | ResponderRole
+                     deriving Eq
+
+-- | Represents the various options which define a handshake.
+data HandshakeOpts d =
+  HandshakeOpts { _hoRole                :: HandshakeRole
+                , _hoPrologue            :: Plaintext
+                , _hoLocalStatic         :: Maybe (KeyPair d)
+                , _hoLocalEphemeral      :: Maybe (KeyPair d)
+                , _hoRemoteStatic        :: Maybe (PublicKey d)
+                , _hoRemoteEphemeral     :: Maybe (PublicKey d)
+                }
+
+$(makeLenses ''HandshakeOpts)
+
+-- | Holds all state associated with the interpreter.
+data HandshakeState c d h =
+  HandshakeState { _hsSymmetricState :: SymmetricState c h
+                 , _hsOpts           :: HandshakeOpts d
+                 , _hsMsgBuffer      :: ScrubbedBytes
+                 }
+
+$(makeLenses ''HandshakeState)
+
+
+-- | All HandshakePattern interpreters run within this Monad.
+newtype Handshake c d h r =
+  Handshake { runHandshake :: Coroutine (Request ScrubbedBytes ScrubbedBytes) (StateT (HandshakeState c d h) Catch) r
+            } deriving ( Functor
+                       , Applicative
+                       , Monad
+                       , MonadThrow
+                       , MonadState (HandshakeState c d h)
+                       )
+
+-- | Returns a default set of handshake options. The prologue is set to an
+--   empty string and all keys are set to 'Nothing'.
+defaultHandshakeOpts :: HandshakeRole
+                     -> HandshakeOpts d
+defaultHandshakeOpts r =
+  HandshakeOpts { _hoRole                = r
+                , _hoPrologue            = mempty
+                , _hoLocalStatic         = Nothing
+                , _hoLocalEphemeral      = Nothing
+                , _hoRemoteStatic        = Nothing
+                , _hoRemoteEphemeral     = Nothing
+                }
+
+-- | Given a protocol name, returns the full handshake name according to the
+--   rules in section 8.
+mkHandshakeName :: forall c d h proxy. (Cipher c, DH d, Hash h)
+                => ByteString
+                -> proxy (c, d, h)
+                -> ScrubbedBytes
+mkHandshakeName protoName _ =
+  "Noise_" <> convert protoName <> "_" <> d <> "_" <> c <> "_" <> h
+  where
+    c = cipherName (Proxy :: Proxy c)
+    d = dhName     (Proxy :: Proxy d)
+    h = hashName   (Proxy :: Proxy h)
+
+-- | Constructs a HandshakeState from a given set of options and a protocol
+--   name (such as "NN" or "IK").
+handshakeState :: forall c d h. (Cipher c, DH d, Hash h)
+               => HandshakeOpts d
+               -> ByteString
+               -> HandshakeState c d h
+handshakeState ho protoName =
+  HandshakeState { _hsSymmetricState = ss'
+                 , _hsOpts           = ho
+                 , _hsMsgBuffer      = mempty
+                 }
+  where
+    ss  = symmetricState $ mkHandshakeName protoName
+                                           (Proxy :: Proxy (c, d, h))
+    ss' = mixHash (ho ^. hoPrologue) ss
+
+instance (Functor f, MonadThrow m) => MonadThrow (Coroutine f m) where
+  throwM = lift . throwM
+
+instance (Functor f, MonadState s m) => MonadState s (Coroutine f m) where
+  get = lift get
+  put = lift . put
+  state = lift . state

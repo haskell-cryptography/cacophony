30b73f0d53a55555c46c7713904c113c4519020f
John Galt <centromere@users.noreply.github.com>
2017-06-12 00:33:30 -0400

    Bring benchmarks up to date

--- benchmarks/Types.hs
+++ benchmarks/Types.hs
@@ -1,83 +1,190 @@
-{-# LANGUAGE RankNTypes, KindSignatures, GADTs #-}
+{-# LANGUAGE RecordWildCards, RankNTypes, KindSignatures, GADTs #-}
 module Types where
 
-import Data.ByteArray        (convert)
-import Data.ByteString.Char8 (unpack)
+import Data.Monoid ((<>))
 
+import Crypto.Noise
 import Crypto.Noise.Cipher
 import Crypto.Noise.Cipher.ChaChaPoly1305
 import Crypto.Noise.Cipher.AESGCM
 import Crypto.Noise.DH
 import Crypto.Noise.DH.Curve25519
 import Crypto.Noise.DH.Curve448
-import Crypto.Noise.Hash
+import Crypto.Noise.HandshakePatterns
+import Crypto.Noise.Hash hiding (hash)
 import Crypto.Noise.Hash.SHA256
 import Crypto.Noise.Hash.SHA512
 import Crypto.Noise.Hash.BLAKE2s
 import Crypto.Noise.Hash.BLAKE2b
 
-data HandshakeType = NoiseNN
-                   | NoiseKN
-                   | NoiseNK
-                   | NoiseKK
-                   | NoiseNX
-                   | NoiseKX
-                   | NoiseXN
-                   | NoiseIN
-                   | NoiseXK
-                   | NoiseIK
-                   | NoiseXX
-                   | NoiseIX
-                   | NoiseN
-                   | NoiseK
-                   | NoiseX
-                   deriving Eq
+data PatternName
+  = PatternNN
+  | PatternKN
+  | PatternNK
+  | PatternKK
+  | PatternNX
+  | PatternKX
+  | PatternXN
+  | PatternIN
+  | PatternXK
+  | PatternIK
+  | PatternXX
+  | PatternIX
+  | PatternN
+  | PatternK
+  | PatternX
+  | PatternNNpsk0
+  | PatternNNpsk2
+  | PatternNKpsk0
+  | PatternNKpsk2
+  | PatternNXpsk2
+  | PatternXNpsk3
+  | PatternXKpsk3
+  | PatternXXpsk3
+  | PatternKNpsk0
+  | PatternKNpsk2
+  | PatternKKpsk0
+  | PatternKKpsk2
+  | PatternKXpsk2
+  | PatternINpsk1
+  | PatternINpsk2
+  | PatternIKpsk1
+  | PatternIKpsk2
+  | PatternIXpsk2
+  | PatternNpsk0
+  | PatternKpsk0
+  | PatternXpsk1
+  deriving (Eq, Enum, Bounded)
+
+instance Show PatternName where
+  show PatternNN = "NN"
+  show PatternKN = "KN"
+  show PatternNK = "NK"
+  show PatternKK = "KK"
+  show PatternNX = "NX"
+  show PatternKX = "KX"
+  show PatternXN = "XN"
+  show PatternIN = "IN"
+  show PatternXK = "XK"
+  show PatternIK = "IK"
+  show PatternXX = "XX"
+  show PatternIX = "IX"
+  show PatternN  = "N"
+  show PatternK  = "K"
+  show PatternX  = "X"
+  show PatternNNpsk0 = "NNpsk0"
+  show PatternNNpsk2 = "NNpsk2"
+  show PatternNKpsk0 = "NKpsk0"
+  show PatternNKpsk2 = "NKpsk2"
+  show PatternNXpsk2 = "NXpsk2"
+  show PatternXNpsk3 = "XNpsk3"
+  show PatternXKpsk3 = "XKpsk3"
+  show PatternXXpsk3 = "XXpsk3"
+  show PatternKNpsk0 = "KNpsk0"
+  show PatternKNpsk2 = "KNpsk2"
+  show PatternKKpsk0 = "KKpsk0"
+  show PatternKKpsk2 = "KKpsk2"
+  show PatternKXpsk2 = "KXpsk2"
+  show PatternINpsk1 = "INpsk1"
+  show PatternINpsk2 = "INpsk2"
+  show PatternIKpsk1 = "IKpsk1"
+  show PatternIKpsk2 = "IKpsk2"
+  show PatternIXpsk2 = "IXpsk2"
+  show PatternNpsk0  = "Npsk0"
+  show PatternKpsk0  = "Kpsk0"
+  show PatternXpsk1  = "Xpsk1"
+
+data HandshakeName = HandshakeName
+  { hsPatternName :: PatternName
+  , hsCipher      :: SomeCipherType
+  , hsDH          :: SomeDHType
+  , hsHash        :: SomeHashType
+  }
+
+instance Show HandshakeName where
+  show HandshakeName{..} = "Noise_"
+                          <> show hsPatternName
+                          <> "_"
+                          <> show hsDH
+                          <> "_"
+                          <> show hsCipher
+                          <> "_"
+                          <> show hsHash
 
 data CipherType :: * -> * where
-  CTChaChaPoly1305 :: CipherType ChaChaPoly1305
-  CTAESGCM         :: CipherType AESGCM
+  ChaChaPoly1305 :: CipherType ChaChaPoly1305
+  AESGCM         :: CipherType AESGCM
 
 data SomeCipherType where
   WrapCipherType :: forall c. Cipher c => CipherType c -> SomeCipherType
 
+instance Show SomeCipherType where
+  show (WrapCipherType ChaChaPoly1305) = "ChaChaPoly"
+  show (WrapCipherType AESGCM)         = "AESGCM"
+
 data DHType :: * -> * where
-  DTCurve25519 :: DHType Curve25519
-  DTCurve448   :: DHType Curve448
+  Curve25519 :: DHType Curve25519
+  Curve448   :: DHType Curve448
 
 data SomeDHType where
   WrapDHType :: forall d. DH d => DHType d -> SomeDHType
 
+instance Show SomeDHType where
+  show (WrapDHType Curve25519) = "25519"
+  show (WrapDHType Curve448)   = "448"
+
 data HashType :: * -> * where
-  HTSHA256  :: HashType SHA256
-  HTSHA512  :: HashType SHA512
-  HTBLAKE2s :: HashType BLAKE2s
-  HTBLAKE2b :: HashType BLAKE2b
+  BLAKE2b :: HashType BLAKE2b
+  BLAKE2s :: HashType BLAKE2s
+  SHA256  :: HashType SHA256
+  SHA512  :: HashType SHA512
 
 data SomeHashType where
   WrapHashType :: forall h. Hash h => HashType h -> SomeHashType
 
-instance Show HandshakeType where
-  show NoiseNN = "NN"
-  show NoiseKN = "KN"
-  show NoiseNK = "NK"
-  show NoiseKK = "KK"
-  show NoiseNX = "NX"
-  show NoiseKX = "KX"
-  show NoiseXN = "XN"
-  show NoiseIN = "IN"
-  show NoiseXK = "XK"
-  show NoiseIK = "IK"
-  show NoiseXX = "XX"
-  show NoiseIX = "IX"
-  show NoiseN  = "N"
-  show NoiseK  = "K"
-  show NoiseX  = "X"
+instance Show SomeHashType where
+  show (WrapHashType BLAKE2b) = "BLAKE2b"
+  show (WrapHashType BLAKE2s) = "BLAKE2s"
+  show (WrapHashType SHA256)  = "SHA256"
+  show (WrapHashType SHA512)  = "SHA512"
 
-instance Cipher c => Show (CipherType c) where
-  show = unpack . convert . cipherName
+patternToHandshake :: PatternName
+                   -> HandshakePattern
+patternToHandshake PatternNN = noiseNN
+patternToHandshake PatternKN = noiseKN
+patternToHandshake PatternNK = noiseNK
+patternToHandshake PatternKK = noiseKK
+patternToHandshake PatternNX = noiseNX
+patternToHandshake PatternKX = noiseKX
+patternToHandshake PatternXN = noiseXN
+patternToHandshake PatternIN = noiseIN
+patternToHandshake PatternXK = noiseXK
+patternToHandshake PatternIK = noiseIK
+patternToHandshake PatternXX = noiseXX
+patternToHandshake PatternIX = noiseIX
+patternToHandshake PatternN  = noiseN
+patternToHandshake PatternK  = noiseK
+patternToHandshake PatternX  = noiseX
+patternToHandshake PatternNNpsk0 = noiseNNpsk0
+patternToHandshake PatternNNpsk2 = noiseNNpsk2
+patternToHandshake PatternNKpsk0 = noiseNKpsk0
+patternToHandshake PatternNKpsk2 = noiseNKpsk2
+patternToHandshake PatternNXpsk2 = noiseNXpsk2
+patternToHandshake PatternXNpsk3 = noiseXNpsk3
+patternToHandshake PatternXKpsk3 = noiseXKpsk3
+patternToHandshake PatternXXpsk3 = noiseXXpsk3
+patternToHandshake PatternKNpsk0 = noiseKNpsk0
+patternToHandshake PatternKNpsk2 = noiseKNpsk2
+patternToHandshake PatternKKpsk0 = noiseKKpsk0
+patternToHandshake PatternKKpsk2 = noiseKKpsk2
+patternToHandshake PatternKXpsk2 = noiseKXpsk2
+patternToHandshake PatternINpsk1 = noiseINpsk1
+patternToHandshake PatternINpsk2 = noiseINpsk2
+patternToHandshake PatternIKpsk1 = noiseIKpsk1
+patternToHandshake PatternIKpsk2 = noiseIKpsk2
+patternToHandshake PatternIXpsk2 = noiseIXpsk2
+patternToHandshake PatternNpsk0  = noiseNpsk0
+patternToHandshake PatternKpsk0  = noiseKpsk0
+patternToHandshake PatternXpsk1  = noiseXpsk1
 
-instance DH d => Show (DHType d) where
-  show = unpack . convert . dhName
 
-instance Hash h => Show (HashType h) where
-  show = unpack . convert . hashName
--- benchmarks/bench.hs
+++ benchmarks/bench.hs
@@ -1,142 +1,130 @@
-{-# LANGUAGE OverloadedStrings, GADTs #-}
+{-# LANGUAGE GADTs #-}
 module Main where
 
+import Control.Exception (SomeException)
+import Control.Lens
 import Criterion.Main
-import Data.ByteArray         (ScrubbedBytes, convert)
-import Data.ByteString        (ByteString)
-import Data.ByteString.Base16 (decode)
-import Data.List              (replicate)
-import Data.Maybe             (fromJust)
-import Data.Monoid            ((<>))
-import Prelude hiding         (replicate)
+import Data.ByteArray    (ScrubbedBytes)
+import Data.Monoid       ((<>))
 
 import Crypto.Noise
 import Crypto.Noise.Cipher
 import Crypto.Noise.DH
-import Crypto.Noise.Hash
+import Crypto.Noise.Hash hiding (hash)
 
-import Handshakes
+import Keys
 import Types
 
-hexToScrubbedBytes :: ByteString
-                   -> ScrubbedBytes
-hexToScrubbedBytes = convert . fst . decode
+genMessage :: (Cipher c, DH d, Hash h)
+           => Bool                -- ^ True if we are writing a message
+           -> Maybe ScrubbedBytes -- ^ If a PSK is called for, this value will be used
+           -> ScrubbedBytes       -- ^ The payload to write/read
+           -> NoiseState c d h    -- ^ The NoiseState to use
+           -> Either SomeException (ScrubbedBytes, NoiseState c d h)
+genMessage write mpsk payload state = do
+  noiseResult <- operation payload state
+  case noiseResult of
+    (ResultMessage m, s) -> pure (m, s)
+    (ResultNeedPSK, s) -> case mpsk of
+      Nothing -> Left . error $ "PSK requested but none has been configured"
+      Just k  -> genMessage write mpsk k s
 
-hexToPair :: DH d
-          => ByteString
-          -> KeyPair d
-hexToPair = fromJust . dhBytesToPair . convert . fst . decode
-
-initiatorEphemeral :: DH d
-                   => DHType d
-                   -> KeyPair d
-initiatorEphemeral DTCurve25519 = hexToPair "893e28b9dc6ca8d611ab664754b8ceb7bac5117349a4439a6b0569da977c464a"
-initiatorEphemeral DTCurve448 = hexToPair "7fd26c8b8a0d5c98c85ff9ca1d7bc66d78578b9f2c4c170850748b27992767e6ea6cc9992a561c9d19dfc342e260c280ef4f3f9b8f879d4e"
-
-responderEphemeral :: DH d
-                   => DHType d
-                   -> KeyPair d
-responderEphemeral DTCurve25519 = hexToPair "bbdb4cdbd309f1a1f2e1456967fe288cadd6f712d65dc7b7793d5e63da6b375b"
-responderEphemeral DTCurve448 = hexToPair "3facf7503ebee252465689f1d4e3b1dd219639ef9de4ffd6049d6d71a0f62126840febb99042421ce12af6626d98d9170260390fbc8399a5"
-
-initiatorStatic :: DH d
-                => DHType d
-                -> KeyPair d
-initiatorStatic DTCurve25519 = hexToPair "e61ef9919cde45dd5f82166404bd08e38bceb5dfdfded0a34c8df7ed542214d1"
-initiatorStatic DTCurve448 = hexToPair "34d564c4be963d1b2a89fcfe83e6a72b5e3f5e3127f9f596ffc7575e418dfc1f4e827cfc10c9fed38e92ad56ddf8f08571430df2e76d5411"
-
-responderStatic :: DH d
-                => DHType d
-                -> KeyPair d
-responderStatic DTCurve25519 = hexToPair "4a3acbfdb163dec651dfa3194dece676d437029c62a408b4c5ea9114246e4893"
-responderStatic DTCurve448 = hexToPair "a9b45971180882a79b89a3399544a425ef8136d278efa443ed67d3ff9d36e883bc330c6295bbf6ed73ff6fd10cbed767ad05ce03ebd27c7c"
-
-mkKeys :: DH d
-       => ScrubbedBytes
-       -> Maybe ScrubbedBytes
-       -> Bool
-       -> DHType d
-       -> HandshakeKeys d
-mkKeys pro psk i d =
-  HandshakeKeys { hkPrologue       = pro
-                , hkPSK            = psk
-                , hkLocalStatic    = if i then initiatorStatic d else responderStatic d
-                , hkLocalEphemeral = if i then initiatorEphemeral d else responderEphemeral d
-                , hkRemoteStatic   = if i then (snd . responderStatic) d else (snd . initiatorStatic) d
-                }
-
-mkHandshakeName :: HandshakeType
-                -> Maybe ScrubbedBytes
-                -> SomeCipherType
-                -> SomeDHType
-                -> SomeHashType
-                -> String
-mkHandshakeName pat psk (WrapCipherType c) (WrapDHType d) (WrapHashType h) =
-  maybe "Noise_" (const "NoisePSK_") psk <>
-  show pat                               <>
-  "_"                                    <>
-  show d                                 <>
-  "_"                                    <>
-  show c                                 <>
-  "_"                                    <>
-  show h
+  where
+    operation   = if write then writeMessage else readMessage
 
 genMessages :: (Cipher c, DH d, Hash h)
-            => Bool
-            -> NoiseState c d h
-            -> NoiseState c d h
-            -> [ScrubbedBytes]
+            => Bool                -- ^ Set to False for one-way patterns
+            -> NoiseState c d h    -- ^ Initiator NoiseState
+            -> NoiseState c d h    -- ^ Responder NoiseState
+            -> Maybe ScrubbedBytes -- ^ PSK
             -> [(ScrubbedBytes, ScrubbedBytes)]
 genMessages swap = go []
   where
-    go acc _ _ [] = acc
-    go acc sendingState receivingState (payload : rest) =
-      let writeResult           = writeMessage sendingState payload
-          (ct, sendingState')   = either (error "write failed") id writeResult
-          (dp, receivingState') = either (error "read failed")
-                                         id
-                                         $ readMessage receivingState ct in
-      if swap
-        then go (acc <> [(payload, dp)]) receivingState' sendingState' rest
-        else go (acc <> [(payload, dp)]) sendingState' receivingState' rest
+    go acc s r _ | handshakeComplete s && handshakeComplete r = acc
+    go acc sendingState receivingState mpsk =
+      let result = do
+            (ct, sendingState')   <- genMessage True  mpsk "" sendingState
+            (pt, receivingState') <- genMessage False mpsk ct receivingState
+            pure ((pt, ct), sendingState', receivingState')
+      in
 
-mkBench :: (HandshakeType, Int)
-        -> Maybe ScrubbedBytes
-        -> SomeCipherType
+      case result of
+        Right (msg, sendingState', receivingState') ->
+          if swap
+            then go (acc <> [msg]) receivingState' sendingState' mpsk
+            else go (acc <> [msg]) sendingState' receivingState' mpsk
+        Left e -> error $ "exception encountered during message generation: " <> show e
+
+genNoiseStates :: (Cipher c, DH d, Hash h)
+               => CipherType c
+               -> HashType h
+               -> PatternName
+               -> (HandshakeOpts d, HandshakeOpts d)
+               -> (NoiseState c d h, NoiseState c d h)
+genNoiseStates _ _ pat (iopts, ropts) =
+  (noiseState iopts hs, noiseState ropts hs)
+  where
+    hs = patternToHandshake pat
+
+genOpts :: DH d
+        => DHType d
+        -> PatternName
+        -> (HandshakeOpts d, HandshakeOpts d)
+genOpts d pat = (iopts, ropts)
+  where
+    idho  = defaultHandshakeOpts InitiatorRole
+    rdho  = defaultHandshakeOpts ResponderRole
+    keys  = getKeys (WrapDHType d) pat
+    iopts = idho & hoPrologue       .~ ""
+                 & hoLocalEphemeral .~ (dhBytesToPair =<< hskInitEphemeral    keys)
+                 & hoLocalStatic    .~ (dhBytesToPair =<< hskInitStatic       keys)
+                 & hoRemoteStatic   .~ (dhBytesToPub  =<< hskInitRemoteStatic keys)
+
+    ropts = rdho & hoPrologue       .~ ""
+                 & hoLocalEphemeral .~ (dhBytesToPair =<< hskRespEphemeral    keys)
+                 & hoLocalStatic    .~ (dhBytesToPair =<< hskRespStatic       keys)
+                 & hoRemoteStatic   .~ (dhBytesToPub  =<< hskRespRemoteStatic keys)
+
+toBench :: SomeCipherType
         -> SomeDHType
         -> SomeHashType
+        -> PatternName
         -> [(ScrubbedBytes, ScrubbedBytes)]
-mkBench (pat, numMsgs) psk (WrapCipherType c) (WrapDHType d) (WrapHashType h) =
-  let ihk = mkKeys "" psk True d
-      rhk = mkKeys "" psk False d
-      ins = mkNoiseState ihk pat InitiatorRole c h
-      rns = mkNoiseState rhk pat ResponderRole c h
-      swap = not (pat == NoiseN || pat == NoiseK || pat == NoiseX) in
+toBench (WrapCipherType c) (WrapDHType d) (WrapHashType h) pat =
+  genMessages swap ins rns (Just psk)
+  where
+    psk        = "This is my Austrian perspective!"
+    swap       = pat /= PatternN && pat /= PatternK && pat /= PatternX &&
+                 pat /= PatternNpsk0 && pat /= PatternKpsk0 && pat /= PatternXpsk1
+    opts       = genOpts d pat
+    (ins, rns) = genNoiseStates c h pat opts
 
-  genMessages swap ins rns $ replicate numMsgs ""
+allHandshakes :: [HandshakeName]
+allHandshakes = do
+  pattern <- [minBound .. maxBound]
+
+  cipher  <- [ WrapCipherType AESGCM
+             , WrapCipherType ChaChaPoly1305
+             ]
+
+  dh      <- [ WrapDHType Curve25519
+             , WrapDHType Curve448
+             ]
+
+  hash    <- [ WrapHashType BLAKE2b
+             , WrapHashType BLAKE2s
+             , WrapHashType SHA256
+             , WrapHashType SHA512
+             ]
+
+  return $ HandshakeName pattern cipher dh hash
 
 main :: IO ()
 main = do
-  let patterns = [ (NoiseNN, 2)
-                 , (NoiseKN, 2)
-                 , (NoiseNK, 2)
-                 , (NoiseKK, 2)
-                 , (NoiseNX, 2)
-                 , (NoiseKX, 2)
-                 , (NoiseXN, 3)
-                 , (NoiseIN, 2)
-                 , (NoiseXK, 3)
-                 , (NoiseIK, 2)
-                 , (NoiseXX, 3)
-                 , (NoiseIX, 2)
-                 , (NoiseN, 1)
-                 , (NoiseK, 1)
-                 , (NoiseX, 1)
-                 ]
-      psks     = [Nothing, Just "This is my Austrian perspective!"]
-      ciphers  = [WrapCipherType CTChaChaPoly1305, WrapCipherType CTAESGCM]
-      dhs      = [WrapDHType DTCurve25519, WrapDHType DTCurve448]
-      hashes   = [WrapHashType HTSHA256, WrapHashType HTSHA512, WrapHashType HTBLAKE2s, WrapHashType HTBLAKE2b]
-      benches  = [bench (mkHandshakeName pat psk c d h) (nf (mkBench patPair psk c d) h) | patPair@(pat, _) <- patterns, psk <- psks, c <- ciphers, d <- dhs, h <- hashes]
+  let benches = do
+        hs <- allHandshakes
+        let b   = toBench (hsCipher hs) (hsDH hs) (hsHash hs)
+            pat = hsPatternName hs
+        return $ bench (show hs) (nf b pat)
 
   defaultMain benches
